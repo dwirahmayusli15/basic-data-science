01cdb48bd4f7ae4a88ec6d45bc9da3afbfa18e09
dwirahmayusli15 <dwirahmaayusli@gmail.com>
2020-04-26 13:24:42 +0700

    testing.py

---
+++ testing.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import pylab as pl
+import numpy as np
+%matplotlib inline
+
+import pandas as pd
+data = pd.io.parsers.read_csv("E:\\studentperformance.csv")
+df = pd.read_csv("E:\\studentperformance.csv")
+df.head(51)
