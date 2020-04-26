93b12c79e7186f5422029e7ae77a65d16bcfeb21
Dwi Rahma Yusli <55719637+dwirahmayusli15@users.noreply.github.com>
2020-04-26 13:30:54 +0700

    Update testing.py

--- testing.py
+++ testing.py
@@ -5,6 +5,6 @@ import numpy as np
 %matplotlib inline
 
 import pandas as pd
-data = pd.io.parsers.read_csv("E:\\studentperformance.csv")
-df = pd.read_csv("E:\\studentperformance.csv")
+data = pd.io.parsers.read_csv("/home/dwirahmayusli/Documents/BigData/studentperformance.csv")
+df = pd.read_csv("/home/dwirahmayusli/Documents/BigData/studentperformance.csv")
 df.head(51)
